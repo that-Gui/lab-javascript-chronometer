69f23b6b9af57f186d050c5652b650a1e39bf0a9
thatGui <gui@thatgui.com>
2022-01-24 18:13:47 +0000

    commit

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,42 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
-
+  
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if ( callback ) return callback;
+      this.currentTime++;
+    }, 1000);
+    //if ( !callback ) return;
   }
 
   getMinutes() {
-    // ... your code goes here
+    if ( this.currentTime < 60) return 0;
+    return Math.floor(this.currentTime / 60);
   }
-
+  
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if ( value < 10 ) return `0${value}`;
+    if ( value > 10 ) return `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    
+    return `${this.computeTwoDigitNumber(this.getMinutes(this.currentTime))}:${this.computeTwoDigitNumber(this.getSeconds(this.currentTime))}`;
   }
 }
 
